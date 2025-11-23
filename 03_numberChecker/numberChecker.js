71bf56188a3040a1e9709e14e393b1113ace8075
b-bet <240735122+b-bet@users.noreply.github.com>
2025-11-23 13:47:50 -0600

    11.23 - 1

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,21 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 10) {
     return true;
-  } else {
-    return false;
   }
+
+  return false;
 }
 
 // Do not edit below this line
 module.exports = numberChecker;
+
+function numberdivider(number) {
+  if (number >= 10) {
+    return number / 2;
+  }
+  else {
+  return number / 1;
+}
+}
+
+module.exports.numberdivider = numberdivider;
